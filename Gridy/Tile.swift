4d09ad14a79a7a0888968111d80f317d9db8fb8e
Matt Sousa <mattsousatwo@gmail.com>
2019-04-23 16:37:58 -0400

    Added Tiles Class -  Initializer for OriginalTileLocation, correctPosition and isInCorrectPosition variables

---
+++ Gridy/Tile.swift
@@ -0,0 +1,35 @@
+//
+//  Tile.swift
+//  Gridy
+//
+//  Created by Matthew Sousa on 4/23/19.
+//  Copyright © 2019 Matthew Sousa. All rights reserved.
+//
+
+import UIKit
+
+class Tile : UIImageView {
+    
+    var originalTileLocation: CGPoint?
+    
+    // final position in grid 0 - 15
+    // access using gridLocations array
+    var correctPosition: Int
+    
+    var isInCorrectPosition: Bool
+    
+    init(originalTileLocation: CGPoint?, correctPosition: Int, frame: CGRect) {
+        self.originalTileLocation = originalTileLocation
+        self.correctPosition = correctPosition
+        self.isInCorrectPosition = false
+        super.init(frame: frame)
+    }
+    
+    
+    // required init by UIImageView
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
